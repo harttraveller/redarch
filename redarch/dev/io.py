d688b2037a6cbce7b25d2a055f60fc3410b4bca7
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-16 00:16:11 -0700

    redarch/dev/io: use cool int

--- redarch/dev/io.py
+++ redarch/dev/io.py
@@ -8,7 +8,7 @@ from zstandard import ZstdDecompressor
 
 class ZSTJSONL:
     def __init__(self, path: str, size: int = 1 << 20) -> None:
-        self.stream = ZstdDecompressor(max_window_size=int(2**31)).stream_reader(
+        self.stream = ZstdDecompressor(max_window_size=1 << 31).stream_reader(
             open(path, "rb")
         )
         self.size = size
