011f34c9fd532b6b79091f616ce4a0788457c497
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-23 15:38:36 -0700

    redarch/db/model: copy over code

--- redarch/db/model.py
+++ redarch/db/model.py
@@ -1,7 +1,13 @@
+from typing import Optional
 from sqlmodel import SQLModel, Session, Field, create_engine
 
 
-class Subreddit(SQLModel, table=True): ...
+class Subreddit(SQLModel, table=True):
+    id: Optional[int] = Field(
+        default=None,
+        primary_key=True,
+        description="Primary database key used by SQLite.",
+    )
 
 
 class Submission(SQLModel, table=True): ...
